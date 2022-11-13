df56b16601389e93ef21f4fddca9569157412804
JacobElbaz <jacobel@post.bgu.ac.il>
2022-11-13 15:13:43 +0200

    typescript converting

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 
-const dotenv = require('dotenv').config()
+import dotenv from 'dotenv'
+dotenv.config()
 
-const bodyParser = require('body-parser')
+import bodyParser from 'body-parser'
 app.use(bodyParser.urlencoded({ extended: true, limit: '1mb'}))
 app.use(bodyParser.json())
 
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser:true })
 const db = mongoose.connection
 db.on('error', error => {console.error(error)})
 db.once('open', () => {console.log('connected to mongo')})
 
-const postRouter = require('./routes/post.js')
+import postRouter from './routes/post.js'
 app.use('/post', postRouter)
 
 module.exports = app
